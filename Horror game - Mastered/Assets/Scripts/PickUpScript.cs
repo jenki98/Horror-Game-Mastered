5cd166abe45d3bfe612d4da38975f7895533d190
jenki98 <zaynab.bawa@gmail.com>
2023-01-07 18:22:03 +0000

    New section + PickUp
    feat: new section added and pickup/interaction added using unity's input system.
    
    Added:
    PickUpScript.cs
    
    Updated:
    StarterAssetsInput.cs
    SampleScene.Unity

---
+++ Horror game - Mastered/Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,28 @@
+﻿using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PickUpScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private StarterAssetsInputs input;
+
+    private void Start()
+    {
+        input = player.GetComponent<StarterAssetsInputs>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (input.interact)
+            {
+                this.gameObject.SetActive(false);
+            }
+               
+        }
+        
+    }
+}
